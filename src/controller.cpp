c9849d1b9926708b6d973b6d326162e80182d0fb
Archengius <Archengius@gmail.com>
2019-11-27 19:56:02 +0700

    Added BootstrapModule() call when module is loaded by bootstrapper controller.

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,28 +1,38 @@
-#include <Windows.h>
-#include <iostream>
 #include "logging.h"
 #include "DllLoader.h"
 
-#define MAX_MODULE_PATH_SIZE 500
+typedef HMODULE (*LoadModuleFunc)(const char* filePath);
+typedef FARPROC (*BootstrapModuleFunc)(LoadModuleFunc loadModuleFuncPtr);
+
+static DllLoader* dllLoader;
+
+HMODULE LoadModuleInternal(const char* filePath) {
+    if (dllLoader == nullptr) {
+        Logging::logFile << "WARNING: loadPatchModule called before dllLoader was initialized!" << std::endl;
+        return nullptr;
+    }
+    Logging::logFile << "Attempting to load module: " << filePath << std::endl;
+    return dllLoader->LoadModule(filePath);
+}
 
 void setupExecutableHook() {
     Logging::initializeLogging();
     Logging::logFile << "Setting up hooking" << std::endl;
 
-
-    HMODULE hGamePrimaryModule = GetModuleHandleA("FactoryGame-Win64-Shipping.exe");
     try {
-        ImportResolver* resolver = new ImportResolver("FactoryGame-Win64-Shipping.exe");
-        DllLoader* dllLoader = new DllLoader(resolver);
-        dllLoader->LoadModule("UE4-ExampleMod-Win64-Shipping.dll");
-    } catch (std::exception ex) {
+        auto* resolver = new ImportResolver("FactoryGame-Win64-Shipping.exe");
+        dllLoader = new DllLoader(resolver);
+        HMODULE loadedModule = dllLoader->LoadModule("UE4-ExampleMod-Win64-Shipping.dll");
+        FARPROC bootstrapFunc = GetProcAddress(loadedModule, "BootstrapModule");
+        if (bootstrapFunc == nullptr) {
+            Logging::logFile << "BootstrapModule() not found in loaded module!";
+            return;
+        }
+        Logging::logFile << "Calling BootstrapModule() on init module" << std::endl;
+        ((BootstrapModuleFunc) bootstrapFunc)(&LoadModuleInternal);
+    } catch (std::exception& ex) {
         Logging::logFile << "Failed to initialize import resolver: " << ex.what() << std::endl;
         exit(1);
     }
-
-   // std::string exampleSymbol("agsDeInit");
-    //void* agsDeInitAddress = resolver->ResolveSymbol(exampleSymbol);
-  //  std::cout << "agsDeInit Address: << " << agsDeInitAddress << std::endl;
-   // delete resolver;
 }
 
--- src/controller.h
+++ src/controller.h
@@ -1,6 +1,7 @@
 #ifndef XINPUT1_3_CONTROLLER_H
 #define XINPUT1_3_CONTROLLER_H
 
+#include <Windows.h>
 #include <iostream>
 
 void setupExecutableHook();
